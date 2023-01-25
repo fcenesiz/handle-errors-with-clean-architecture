0fc1f6c84b34fbacfd171082377632d1ad29f3b8
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-25 11:40:12 +0300

    view initialized

--- app/src/main/java/com/fcenesiz/handleerrors/MainActivity.kt
+++ app/src/main/java/com/fcenesiz/handleerrors/MainActivity.kt
@@ -3,25 +3,48 @@ package com.fcenesiz.handleerrors
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fcenesiz.handleerrors.presentation.MainViewModel
 import com.fcenesiz.handleerrors.ui.theme.HandleErrorsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel: MainViewModel by viewModels()
         setContent {
             HandleErrorsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        OutlinedTextField(
+                            value = viewModel.email,
+                            onValueChange = viewModel::onEmailChanged,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            onClick = viewModel::submitEmail,
+                            modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Text(text = "Submit")
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        if (viewModel.error != null) {
+                            Text(text = viewModel.error ?: "")
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/fcenesiz/handleerrors/presentation/MainViewModel.kt
+++ app/src/main/java/com/fcenesiz/handleerrors/presentation/MainViewModel.kt
@@ -3,8 +3,15 @@ package com.fcenesiz.handleerrors.presentation
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fcenesiz.handleerrors.domain.SubmitEmailUseCase
+import com.fcenesiz.handleerrors.util.Resource
+import kotlinx.coroutines.launch
 
-class MainViewModel {
+class MainViewModel(
+    private val submitEmailUseCase: SubmitEmailUseCase = SubmitEmailUseCase()
+) : ViewModel() {
 
     var email by mutableStateOf("")
         private set
@@ -12,8 +19,22 @@ class MainViewModel {
     var error by mutableStateOf<String?>(null)
         private set
 
-    fun onEmailChanged(email: String){
+    fun onEmailChanged(email: String) {
         this.email = email
+    }
+
+    fun submitEmail() {
+        viewModelScope.launch {
+            val result = submitEmailUseCase.execute(email)
+            when (result) {
+                is Resource.Success -> {
+
+                }
+                is Resource.Error -> {
+                    error = result.message
+                }
+            }
+        }
     }
 
 }
